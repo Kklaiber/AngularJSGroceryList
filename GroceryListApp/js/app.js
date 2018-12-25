90036385e5260f7150161e6d3d72fb7e1ddacf01
Kyle Klaiber <Kylesklaiber@gmail.com>
2018-12-24 22:04:10 -0800

    update grocery list

--- GroceryListApp/js/app.js
+++ GroceryListApp/js/app.js
@@ -1,100 +1,104 @@
+
+
+
 var app = angular.module("groceryListApp", ["ngRoute"]);
 
 app.config(function($routeProvider) {
   $routeProvider
     .when("/", {
       templateUrl: "views/groceryList.html",
-      controller: "GroceryListItemsController"
+      controller: "HomeController"
     })
     .when("/addItem", {
       templateUrl: "views/addItem.html",
-      controller: "GroceryListItemsController"
+      controller: "GroceryListItemController"
     })
-    .when("/addItem/:id/:cat", {
+    .when("/addItem/edit/:id", {
       templateUrl: "views/addItem.html",
-      controller: "GroceryListItemsController"
+      controller: "GroceryListItemController"
     })
     .otherwise({
       redirectTo: "/"
     });
 });
 
-app.service("GroceryService", function(){
-    var groceryService = {};
-    groceryService.groceryItems = [
-        {id:1, completed: true, itemName: "milk", date: "2018-12-20" },
-        {id:2, completed: true, itemName: "cookies", date: "2018-12-20" },
-        {id:3, completed: true, itemName: "ice cream", date: "2018-12-10" },
-        {id:4, completed: true, itemName: "potatoes", date: "2018-12-11" },
-        {id:5, completed: true, itemName: "cereal", date: "2018-12-13" },
-        {id:6, completed: true, itemName: "bread", date: "2018-12-13" },
-        {id:7, completed: true, itemName: "eggs", date: "2018-12-13" },
-        {id:8, completed: true, itemName: "tortillas", date: "2018-12-15" }
-      ];
-      groceryService.save = function(entry){
-        groceryService.groceryItems.push(entry);
-      }
-      return groceryService;
-})
+app.service("GroceryService", function() {
+  var groceryService = {};
+  groceryService.groceryItems = [
+    { id: 1, completed: true, itemName: "milk", date: "2018-12-20" },
+    { id: 2, completed: true, itemName: "cookies", date: "2018-12-20" },
+    { id: 3, completed: true, itemName: "ice cream", date: "2018-12-10" },
+    { id: 4, completed: true, itemName: "potatoes", date: "2018-12-11" },
+    { id: 5, completed: true, itemName: "cereal", date: "2018-12-13" },
+    { id: 6, completed: true, itemName: "bread", date: "2018-12-13" },
+    { id: 7, completed: true, itemName: "eggs", date: "2018-12-13" },
+    { id: 8, completed: true, itemName: "tortillas", date: "2018-12-15" }
+  ];
+
+  groceryService.findById = function(id) {
+    for (var i in groceryService.groceryItems) {
+      if (groceryService.groceryItems[i].id === id){
+        return groceryService.groceryItems[i];
+    }
+  }
+  };
+  groceryService.getNewId = function() {
+    if (groceryService.newId) {
+      groceryService.newId++;
+      return groceryService.newId;
+    } else {
+      var maxId = _.max(groceryService.groceryItems, function(entry) {
+        return entry.id;
+      });
+      groceryService.newId = maxId.id + 1;
+      return groceryService.newId;
+    }
+  };
+  groceryService.save = function(entry) { 
+    var updatedItem = groceryService.findById(entry.id);
+
+    if(updatedItem){
+            
+        updatedItem.completed = entry.completed;
+        updatedItem.itemName = entry.itemName;
+        updatedItem.date = entry.date;
+    }else{      
+    entry.id = groceryService.getNewId();
+    groceryService.groceryItems.push(entry);
+  }
+};
+  return groceryService;
+});
 
 app.controller("HomeController", [
   "$scope",
-  function($scope) {
+  "GroceryService",
+  function($scope, GroceryService) {
     $scope.appTitle = "Grocery List";
+    $scope.groceryItems = GroceryService.groceryItems;
   }
 ]);
 
-app.controller("GroceryListItemsController", [
+app.controller("GroceryListItemController", [
   "$scope",
   "$routeParams",
   "$location",
   "GroceryService",
   function($scope, $routeParams, $location, GroceryService) {
-    $scope.groceryItems = GroceryService.groceryItems
-    $scope.groceryItem = { id: 7, completed:true, itemName: "burger", date: new Date()};
-    $scope.save = function(){
-        GroceryService.save($scope.groceryItem);
-        location.pathname("/");
+    if (!$routeParams.id) {
+      $scope.groceryItem = {
+        id: 0,
+        completed: false,
+        itemName: "",
+        date: new Date()
+      };
+    }else{
+        $scope.groceryItem = _.clone(GroceryService.findById(parseInt($routeParams.id)));
     }
+    $scope.save = function() {
+      GroceryService.save($scope.groceryItem);
+      $location.path("/");
+    };
+    // console.log($scope.groceryItems);
   }
 ]);
-
-// var app = angular.module('groceryListApp', ["ngRoute"]);
-
-// app.config(function($routeProvider){
-//     $routeProvider
-//         .when("/",{
-//             templateUrl: "views/groceryList.html",
-//             controller: "GroceryListItemsController"
-//         })
-//         .when("/addItem",{
-//             templateUrl: "views/addItem.html",
-//             controller: "GroceryListItemsController"
-//         })
-//         .when("/addItem/:id", {
-//             templateUrl: "views/addItem.html",
-//             controller: "GroceryListItemsController"
-//         })
-//         .otherwise({
-//             redirectTo: "/"
-//         })
-// });
-
-// app.controller("HomeController", ["$scope", function($scope) {
-//     $scope.appTitle = "Grocery List";
-// }]);
-
-// app.controller("GroceryListItemsController", ["$scope", "$routeParams", function($scope, $routeParams){
-
-//     $scope.groceryItems = [
-//         {completed: true, itemName: 'milk', date: '2014-10-00'},
-//         {completed: true, itemName: 'cookies', date: '2014-10-01'},
-//         {completed: true, itemName: 'ice cream', date: '2014-10-02'},
-//         {completed: true, itemName: 'potatoes', date: '2014-10-02'},
-//         {completed: true, itemName: 'cereal', date: '2014-10-03'},
-//         {completed: true, itemName: 'bread', date: '2014-10-03'},
-//         {completed: true, itemName: 'eggs', date: '2014-10-04'},
-//         {completed: true, itemName: 'tortillas', date: '2014-10-04'}
-//     ]
-
-// }]);
